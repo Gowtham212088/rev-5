c0c11472490f987cba3fcaac649227b3b752cd4f
GOWTHAM KUMAR V <vgowtham.kumar8@gmail.com>
2022-02-01 14:28:18 +0530

    main

--- src/Likes.js
+++ src/Likes.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 import Badge from '@mui/material/Badge';
-
+import { IconButton } from "@mui/material";
 
 
 function Like(){
